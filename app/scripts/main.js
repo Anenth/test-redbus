5bd0637b1cd190137503d48ef8a34fbf90ef56c9
Anenth Vishnu <anenthvishnu@gmail.com>
2013-09-13 09:51:36 +0530

    Today tomo buttons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+var checkInput = function(inputId, type){
+	var errorMsg = 'Please enter a valid city name';
+	if(type === 'date'){
+		errorMsg =  'Please enter a valid date';
+	}
+	if(document.getElementById(inputId).value === '')	{
+		document.getElementById(inputId + 'Error').innerHTML =  errorMsg;
+		document.getElementById(inputId).style.border = '1px solid red';
+	}else{
+		document.getElementById(inputId + 'Error').innerHTML = '' ;
+		document.getElementById(inputId).style.border = '';
+	}
+};
+ 
+var validate = function(){
+	checkInput('inputFromCity', 'city');
+	checkInput('inputToCity', 'city');
+	checkInput('inputDateOfJourney', 'date');
+	return false;
+};
